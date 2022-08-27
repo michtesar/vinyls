81669362695ccdafe0db3ee5a272cf6eb4002bf6
Michael Tesar <michtesar@gmail.com>
2022-08-27 21:23:38 +0200

    Add dark theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,7 @@
-import { useEffect, useState } from "react";
+import CssBaseline from "@mui/material/CssBaseline";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import { useEffect, useMemo, useState } from "react";
 import "./App.css";
 import { SearchBox } from "./components/SearchBox/SearchBox";
 import { SettingsButton } from "./components/SettingsButton/SettingsButton";
@@ -9,6 +12,17 @@ import vinyls from "./vinyls.json";
 function App() {
   const [searchText, setSearchText] = useState("");
   const [filteredVinyls, setFilteredVinyls] = useState<Vinyl[]>([]);
+  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+
+  const theme = useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode: prefersDarkMode ? "dark" : "light",
+        },
+      }),
+    [prefersDarkMode]
+  );
 
   useEffect(() => {
     document.title = "Vinyls";
@@ -23,18 +37,21 @@ function App() {
   }, [searchText]);
 
   return (
-    <div className="App">
-      <h1>Vinyls</h1>
-      <p>Your go to vinyl tracking application</p>
-      <p>{searchText}</p>
-      <SearchBox setSearchText={setSearchText} />
-      <div className="Cards" style={{ display: "inline-flex" }}>
-        {filteredVinyls.map((vinyl) => (
-          <VinylCard {...vinyl} />
-        ))}
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <div className="App">
+        <h1>Vinyls</h1>
+        <p>Your go to vinyl tracking application</p>
+        <p>{searchText}</p>
+        <SearchBox setSearchText={setSearchText} />
+        <div className="Cards" style={{ display: "inline-flex" }}>
+          {filteredVinyls.map((vinyl) => (
+            <VinylCard {...vinyl} />
+          ))}
+        </div>
+        <SettingsButton />
       </div>
-      <SettingsButton />
-    </div>
+    </ThemeProvider>
   );
 }
 
