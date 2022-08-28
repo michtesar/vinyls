14d1a9aad950aa91649752ebca9bf5bcc1a04d9b
Michael Tesar <michtesar@gmail.com>
2022-08-28 15:19:07 +0200

    Add better visuals of the cards

--- src/components/VinylCard/VinylCard.tsx
+++ src/components/VinylCard/VinylCard.tsx
@@ -1,5 +1,4 @@
-import { Card, CardContent, CardHeader, CardMedia } from "@mui/material";
-import Rating from "@mui/material/Rating";
+import { Typography } from "@mui/material";
 import { Vinyl } from "../VinylTypes/Vinyl";
 import "./Card.css";
 
@@ -11,28 +10,44 @@ export const VinylCard = ({
   id,
   rating,
 }: Vinyl) => {
-  const dimension: number = 195;
+  const dimension: number = 150;
 
   return (
-    <Card
-      sx={{
-        width: dimension,
-        ":hover": { boxShadow: "1px 2px 9px grey" },
-      }}
-      className="Card"
-      draggable={true}
-    >
-      <CardHeader key={id} title={album} subheader={artist} />
-      <CardMedia
-        component={"img"}
-        height={dimension}
-        image={cover}
-        alt={artist + "- " + album + "(" + year + ")"}
-        draggable={false}
+    //   <Card
+    //     sx={{
+    //       width: dimension,
+    //       ":hover": { boxShadow: "1px 2px 9px grey" },
+    //     }}
+    //     className="Card"
+    //     draggable={true}
+    //   >
+    //     <CardHeader key={id} title={album} subheader={artist} />
+    //     <CardMedia
+    //       component={"img"}
+    //       height={dimension}
+    //       image={cover}
+    //       alt={artist + "- " + album + "(" + year + ")"}
+    //       draggable={false}
+    //     />
+    //     {/* <CardContent>
+    //       <Rating name="read-only" value={rating} size={"small"} readOnly />
+    //     </CardContent> */}
+    //   </Card>
+    // );
+    <div className="Card">
+      <img
+        alt={artist + " - " + album + "(" + year + ")"}
+        src={cover}
+        width={dimension}
       />
-      <CardContent>
-        <Rating name="read-only" value={rating} size={"small"} readOnly />
-      </CardContent>
-    </Card>
+      <div className="Details" style={{ maxWidth: dimension }}>
+        <Typography className={"Title"} noWrap>
+          {album}
+        </Typography>
+        <Typography className={"Subtitle"} noWrap>
+          {artist}
+        </Typography>
+      </div>
+    </div>
   );
 };
