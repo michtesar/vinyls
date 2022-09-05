ebf0f60c729f990d8884816c91aa791e22568278
Michael Tesar <michtesar@gmail.com>
2022-09-05 20:49:14 +0200

    Add Firebase configuration in config.tsx

---
+++ src/config/config.tsx
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDMnzY4IerFZp55iAFOBBqaRQX6ZgHMXmc",
+    authDomain: "vinyls-76eef.firebaseapp.com",
+    projectId: "vinyls-76eef",
+    storageBucket: "vinyls-76eef.appspot.com",
+    messagingSenderId: "410739116286",
+    appId: "1:410739116286:web:a9105793dcb89141ae7daf",
+    measurementId: "G-BZ08QR2MB8"
+}
