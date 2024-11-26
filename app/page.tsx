df20495a4e3e7df691fc77da00ca7c1afaddfd4b
Michael Tesař <michtesar@gmail.com>
2024-11-26 23:35:12 +0100

    Simplify home page and add navigation links
    Removed fetching logic and vinyl display from the home page to streamline its content. Added navigation links to the 'Collection' and 'Wanted' pages to improve site navigation and user experience.

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,18 @@
 'use client';
 
-import React, { useEffect, useState } from 'react';
-import { fetchWanted, Vinyl } from '@/app/api';
-import { VinylCard } from '@/app/components/vinyl-card';
+import Link from 'next/link';
 
 export default function Home() {
-  const [wanted, setWanted] = useState<Vinyl[] | null>(null);
-
-  useEffect(() => {
-    fetchWanted().then(setWanted);
-  }, []);
-
   return (
     <div className="min-h-screen p-8 pb-20 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 sm:items-start">
-        <h1 className="text-6xl font-bold">Wanted</h1>
-        {wanted ? (
-          <div className="grid">
-            {wanted.map((vinyl) => (
-              <VinylCard vinyl={vinyl} key={vinyl.id} />
-            ))}
-          </div>
-        ) : (
-          <div>Loading..</div>
-        )}
+      <main className="flex flex-col items-start">
+        <h1 className="text-6xl font-bold">Vinyls</h1>
+
+        <h2 className="text-3xl font-bold mt-8">Menu</h2>
+        <nav className="mt-8 flex flex-col gap-4">
+          <Link href={'/collection'}>Collection</Link>
+          <Link href={'/wanted'}>Wanted</Link>
+        </nav>
       </main>
     </div>
   );
