d19e1f5cd4caa9be59dda643df861fd6e377584c
Michael Tesař <michtesar@gmail.com>
2024-11-27 00:05:14 +0100

    Add search input to Collection page
    This commit introduces a search input field to the Collection page, allowing users to filter items by keyword. The search input adjusts its styling based on the user's color scheme preference. This enhances the usability and user experience of the page.

--- app/collection/page.tsx
+++ app/collection/page.tsx
@@ -15,7 +15,19 @@ export default function Home() {
   return (
     <div className="min-h-screen p-8 pb-20 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col items-start">
-        <h1 className="text-6xl font-bold my-5">Collection</h1>
+        <div className="flex justify-between items-center w-full">
+          <h1 className="text-6xl font-bold my-5">Collection</h1>
+          <input
+            type="text"
+            placeholder="Search..."
+            className={`p-2 border rounded-md w-1/4 ${
+              window.matchMedia &&
+              window.matchMedia('(prefers-color-scheme: dark)').matches
+                ? 'bg-gray-800 text-white border-gray-700'
+                : 'bg-white text-black border-gray-300'
+            }`}
+          />
+        </div>
         {collection ? (
           <div className="grid grid-cols-2 xs:grid-cols-3 sm:grid-cols-4 md:grid-cols-6 lg:grid-cols-8 xl:grid-cols-8 2xl:grid-cols-12">
             {collection.map((vinyl) => (
