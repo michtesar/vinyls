7e7ab00a89ec0c9906337647a35a102cc0cf05d7
Michael Tesař <michtesar@gmail.com>
2024-11-26 23:59:29 +0100

    Simplify collection layout structure
    Remove redundant div wrapper around the grid layout within the collection page. This change reduces unnecessary DOM nesting and streamlines the component's structure for better readability and maintainability.

--- app/collection/page.tsx
+++ app/collection/page.tsx
@@ -17,14 +17,12 @@ export default function Home() {
       <main className="flex flex-col items-start">
         <h1 className="text-6xl font-bold">Collection</h1>
         {collection ? (
-          <div className="">
-            <div className="grid grid-cols-2 xs:grid-cols-3 sm:grid-cols-4 md:grid-cols-6 lg:grid-cols-8 xl:grid-cols-8 2xl:grid-cols-12">
-              {collection.map((vinyl) => (
-                <div key={vinyl.id} className="flex flex-col items-center">
-                  <VinylCard vinyl={vinyl} />
-                </div>
-              ))}
-            </div>
+          <div className="grid grid-cols-2 xs:grid-cols-3 sm:grid-cols-4 md:grid-cols-6 lg:grid-cols-8 xl:grid-cols-8 2xl:grid-cols-12">
+            {collection.map((vinyl) => (
+              <div key={vinyl.id} className="flex flex-col items-center">
+                <VinylCard vinyl={vinyl} />
+              </div>
+            ))}
           </div>
         ) : (
           <LoadingSpinner />
