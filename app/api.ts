3e1bbbe524173a992d3be4c0ef3661855f4210e5
Michael Tesař <michtesar@gmail.com>
2024-11-26 23:14:27 +0100

    Add fetchWanted function to retrieve user's wanted vinyls
    Implemented `fetchWanted` in `app/api.ts` to interact with Discogs API, fetching and paginating user's wanted vinyl records. The function transforms the retrieved data into a structured format, including vinyl details such as title, cover image, year, artists, and rating.

---
+++ app/api.ts
@@ -0,0 +1,67 @@
+'use server';
+
+import axios from 'axios';
+
+export interface Vinyl {
+  id: string;
+  title: string;
+  coverUrl: string;
+  year: number;
+  rating: number;
+  artists: Artist[];
+}
+
+export interface Artist {
+  id: number;
+  name: string;
+  resource_url: string;
+}
+
+const userName = 'michtesar';
+const token = process.env.DISCOGS_TOKEN;
+
+export const fetchWanted = async (perPage: number = 10): Promise<Vinyl[]> => {
+  let page = 1;
+  let allWants: Vinyl[] = [];
+  let keepFetching = true;
+
+  try {
+    while (keepFetching) {
+      const response = await axios.get(
+        `https://api.discogs.com/users/${userName}/wants?token=${token}&page=${page}&per_page=${perPage}`,
+      );
+
+      const wants = response.data.wants.map(
+        (item: {
+          id: string;
+          basic_information: {
+            title: string;
+            cover_image: string;
+            year: number;
+            artists: Artist[];
+            rating: number;
+          };
+        }) => ({
+          id: item.id,
+          title: item.basic_information.title,
+          coverUrl: item.basic_information.cover_image,
+          year: item.basic_information.year,
+          rating: item.basic_information.rating,
+          artists: item.basic_information.artists,
+        }),
+      );
+
+      allWants = allWants.concat(wants);
+
+      if (response.data.pagination && response.data.pagination.pages > page) {
+        page++;
+      } else {
+        keepFetching = false;
+      }
+    }
+
+    return allWants;
+  } catch (error) {
+    throw error;
+  }
+};
