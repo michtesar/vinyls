876163b2cc489e7dced33826af2d90bc9521f7e8
Michael Tesař <michtesar@gmail.com>
2024-11-27 00:20:09 +0100

    Remove unused SVG icons and add search filter functionality
    Deleted several unused SVG files from the public directory to clean up the project. Implemented a search filter in the collection page to allow users to filter vinyl records by title and artist names.

--- app/collection/page.tsx
+++ app/collection/page.tsx
@@ -7,10 +7,38 @@ import { LoadingSpinner } from '@/app/components/loading-spinner';
 
 export default function Home() {
   const [collection, setCollection] = useState<Vinyl[] | null>(null);
+  const [filteredCollection, setFilteredCollection] = useState<Vinyl[]>([]);
+  const [search, setSearch] = useState('');
 
   useEffect(() => {
     fetchCollection().then(setCollection);
   }, []);
+
+  useEffect(() => {
+    setFilteredCollection(collection || []);
+  }, [collection]);
+
+  function filterCollection(search: string) {
+    if (search) {
+      setFilteredCollection(
+        collection?.filter(
+          (vinyl) => vinyl.title.toLowerCase().includes(search.toLowerCase()),
+          collection?.filter(
+            (vinyl) =>
+              vinyl.title.toLowerCase().includes(search.toLowerCase()) ||
+              vinyl.artists
+                .map((artist) => artist.name)
+                .join(', ')
+                .split('(')[0]
+                .toLowerCase()
+                .includes(search.toLowerCase()),
+          ) || [],
+        ) || [],
+      );
+    } else {
+      setFilteredCollection(collection || []);
+    }
+  }
 
   return (
     <div className="min-h-screen p-8 pb-20 sm:p-20 font-[family-name:var(--font-geist-sans)]">
@@ -26,11 +54,22 @@ export default function Home() {
                 ? 'bg-gray-800 text-white border-gray-700'
                 : 'bg-white text-black border-gray-300'
             }`}
+            onChange={(e) => {
+              setSearch(e.target.value);
+              filterCollection(search);
+            }}
+            onKeyDown={(e) => {
+              if (e.key === 'Escape') {
+                setSearch('');
+                filterCollection(search);
+              }
+            }}
+            value={search}
           />
         </div>
         {collection ? (
           <div className="grid grid-cols-2 xs:grid-cols-3 sm:grid-cols-4 md:grid-cols-6 lg:grid-cols-8 xl:grid-cols-8 2xl:grid-cols-12">
-            {collection.map((vinyl) => (
+            {filteredCollection.map((vinyl) => (
               <div key={vinyl.id} className="flex flex-col items-center">
                 <VinylCard vinyl={vinyl} />
               </div>
