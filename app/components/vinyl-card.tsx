419e76c66008037ff3fabca892e225d54cef8c30
Michael Tesař <michtesar@gmail.com>
2024-11-26 23:14:41 +0100

    Add VinylCard component
    Introduce the VinylCard component to display vinyl information including the cover image, title, and artists. This component uses Tailwind CSS for styling and Next.js's Image component for optimized image loading.

---
+++ app/components/vinyl-card.tsx
@@ -0,0 +1,30 @@
+import { Vinyl } from '@/app/api';
+import Image from 'next/image';
+import React from 'react';
+
+export const VinylCard = ({ vinyl }: { vinyl: Vinyl }) => {
+  return (
+    <div className="w-1/2 flex flex-col items-center p-4 rounded-lg shadow-lg">
+      <Image
+        src={vinyl.coverUrl}
+        alt={vinyl.title}
+        className="w-full object-cover rounded-md"
+        width="128"
+        height="128"
+      />
+      <div className="w-full mt-4 text-left">
+        <p className="text-white text-xs font-semibold truncate">
+          {vinyl.title}
+        </p>
+        <p className="text-gray-400 text-xs truncate">
+          {
+            vinyl.artists
+              .map((artist) => artist.name)
+              .join(', ')
+              .split('(')[0]
+          }
+        </p>
+      </div>
+    </div>
+  );
+};
