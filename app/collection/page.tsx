5dee1bafb3402052fc1e247d4401b4342b3ed064
Michael Tesař <michtesar@gmail.com>
2024-11-26 23:53:32 +0100

    Simplify grid layout in collection page
    Removed unnecessary column classes to streamline the grid layout. This change allows the grid to default to a single-column layout on smaller screens.

--- app/collection/page.tsx
+++ app/collection/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
       <main className="flex flex-col items-start">
         <h1 className="text-6xl font-bold">Collection</h1>
         {collection ? (
-          <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-8">
+          <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6 mt-8">
             {collection.map((vinyl) => (
               <VinylCard vinyl={vinyl} key={vinyl.id} />
             ))}
