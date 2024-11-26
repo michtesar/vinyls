0c9707a92e331912ee46384c06ddc7bb77405659
Michael Tesař <michtesar@gmail.com>
2024-11-26 23:35:17 +0100

    Add collection page to display user's wanted vinyl records
    This commit introduces a new page to the application that fetches and displays the user's wanted vinyl records using the Discogs API. A loading spinner is shown while the data is being fetched, and the records are displayed in a responsive grid format once loaded.

---
+++ app/collection/page.tsx
@@ -0,0 +1,34 @@
+'use client';
+
+import React, { useEffect, useState } from 'react';
+import { fetchWanted, Vinyl } from '@/app/api';
+import { VinylCard } from '@/app/components/vinyl-card';
+import { userName } from '@/app/config';
+import { LoadingSpinner } from '@/app/components/loading-spinner';
+
+export default function Home() {
+  const [collection, setCollection] = useState<Vinyl[] | null>(null);
+
+  useEffect(() => {
+    fetchWanted(`https://api.discogs.com/users/${userName}/wants`).then(
+      setCollection,
+    );
+  }, []);
+
+  return (
+    <div className="min-h-screen p-8 pb-20 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col items-start">
+        <h1 className="text-6xl font-bold">Collection</h1>
+        {collection ? (
+          <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-8">
+            {collection.map((vinyl) => (
+              <VinylCard vinyl={vinyl} key={vinyl.id} />
+            ))}
+          </div>
+        ) : (
+          <LoadingSpinner />
+        )}
+      </main>
+    </div>
+  );
+}
