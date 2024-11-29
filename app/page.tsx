37961fa4332c3e8f22b95e5e582bf7b1edbecd7f
Michael Tesař <michtesar@gmail.com>
2024-11-29 23:48:49 +0100

    Replace collection page logic in app/page.tsx
    Moved the collection page functionality from app/collection/page.tsx to app/page.tsx. This centralizes the logic and UI components into a single entry point, optimizing the application structure by removing the now redundant file.

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,32 @@
 'use client';
 
-import Link from 'next/link';
+import React, { useState } from 'react';
+import { SearchBar } from '@/app/components/SearchBar';
+import { NavigationLink } from '@/app/components/NavigationLink';
+import { FaGift } from 'react-icons/fa';
+import { Collection } from '@/app/components/Collection';
+import { LoadingSpinner } from '@/app/components/loading-spinner';
+import { useVinylCollection } from '@/app/hooks/useVinylCollection';
+import { useFilteredCollection } from '@/app/hooks/useFilteredCollection';
 
 export default function Home() {
+  const [search, setSearch] = useState('');
+  const vinylCollection = useVinylCollection();
+  const filteredCollection = useFilteredCollection(vinylCollection, search);
+
   return (
     <div className="min-h-screen p-8 pb-20 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col items-start">
-        <h1 className="text-6xl font-bold">Vinyls</h1>
-
-        <h2 className="text-3xl font-bold mt-8">Menu</h2>
-        <nav className="mt-8 flex flex-col gap-4">
-          <Link href={'/collection'}>Collection</Link>
-          <Link href={'/wanted'}>Wanted</Link>
-        </nav>
+        <header className="flex justify-between items-center w-full">
+          <h1 className="text-6xl font-bold my-2">Collection</h1>
+          <SearchBar search={search} setSearch={setSearch} />
+        </header>
+        <NavigationLink label="Wanted" to="/wanted" icon={<FaGift />} />
+        {vinylCollection ? (
+          <Collection vinyls={filteredCollection} />
+        ) : (
+          <LoadingSpinner />
+        )}
       </main>
     </div>
   );
