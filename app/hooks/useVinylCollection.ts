e66dd9aa5d8ccb768d68c5e6ea648e772334bdb0
Michael Tesař <michtesar@gmail.com>
2024-11-29 23:48:07 +0100

    Add JSDoc comments to useVinylCollection hook
    Provide a detailed description for the custom hook managing the vinyl collection. This includes the purpose of the hook and the type of value it returns, enhancing code readability and maintainability.

--- app/hooks/useVinylCollection.ts
+++ app/hooks/useVinylCollection.ts
@@ -1,6 +1,12 @@
 import { useEffect, useState } from 'react';
 import { fetchCollection, Vinyl } from '@/app/api';
 
+/**
+ * Custom hook to manage a vinyl record collection.
+ * It fetches and returns the collection of vinyl records.
+ *
+ * @return {Vinyl[] | null} The current state of the vinyl collection, which is an array of Vinyl objects or null if the collection has not been fetched yet.
+ */
 export function useVinylCollection(): Vinyl[] | null {
   const [collection, setCollection] = useState<Vinyl[] | null>(null);
 
