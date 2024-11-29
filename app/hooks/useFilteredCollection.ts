5d089b68cf91a858d21fc4f31776d69c827d32b9
Michael Tesař <michtesar@gmail.com>
2024-11-29 23:47:45 +0100

    Add JSDoc comments to useFilteredCollection hook
    This commit introduces comprehensive JSDoc comments to the useFilteredCollection hook to enhance code readability and maintainability. The comments describe the parameters, return value, and functionality of the hook, ensuring that future developers can easily understand and utilize the function.

--- app/hooks/useFilteredCollection.ts
+++ app/hooks/useFilteredCollection.ts
@@ -1,6 +1,13 @@
 import { useEffect, useState } from 'react';
 import { Vinyl } from '@/app/api';
 
+/**
+ * Filters the provided collection of Vinyl records based on the search string.
+ *
+ * @param {Vinyl[] | null} collection - The collection of Vinyl records to be filtered.
+ * @param {string} search - The search string used to filter the Vinyl collection. The filter is case-insensitive.
+ * @return {Vinyl[]} The filtered array of Vinyl records matching the search criteria. If no search string is provided, returns the original collection.
+ */
 export function useFilteredCollection(
   collection: Vinyl[] | null,
   search: string,
