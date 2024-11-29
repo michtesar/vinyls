049b217efd26baf94aad57707301d5302020853b
Michael Tesař <michtesar@gmail.com>
2024-11-30 00:46:40 +0100

    Add current path tracking to VinylCard navigation
    Enhance navigation by appending the current pathname to the URL as a query parameter when a VinylCard is clicked. This change allows for easier return navigation by maintaining knowledge of the user's previous page location.

--- app/vinyl/[id]/page.tsx
+++ app/vinyl/[id]/page.tsx
@@ -1,15 +1,37 @@
 'use client';
 
-import { useParams } from 'next/navigation';
+import { useParams, useSearchParams } from 'next/navigation';
 import React from 'react';
+import { useRelease } from '@/app/hooks/useRelease';
+import Image from 'next/image';
+import Link from 'next/link';
 
 const VinylPage = () => {
-  const { id } = useParams();
+  const { id } = useParams() as { id: string };
+  const searchParams = useSearchParams();
+  const backTo = searchParams.get('backTo');
+  const release = useRelease(parseInt(id));
 
   return (
     <div className="container mx-auto p-4">
       <h1 className="text-2xl font-bold mb-4">Vinyl Details</h1>
-      {id ? <p className="text-lg">Vinyl ID: {id}</p> : <p>Loading...</p>}
+      {release ? (
+        <>
+          <p className="text-lg">Title: {release.title}</p>
+          <p className="text-lg">
+            Artist: {release.artists[0].name.split('(')[0]}
+          </p>
+          <Link href={backTo!}>Back</Link>
+          <Image
+            src={release.images[0].resource_url}
+            alt={String(release.id)}
+            width={512}
+            height={512}
+          />
+        </>
+      ) : (
+        <p>Loading...</p>
+      )}
     </div>
   );
 };
